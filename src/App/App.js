6ba995534a677232fe5362c4ef96b2118c57f58e
Kayla Gordon <heykaylagordon@gmail.com>
2024-09-12 09:52:03 -0600

    Complete network request challenge

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Ideas from '../Ideas/Ideas';
 import Form from '../Form/Form';
 import './App.css';
 
 export default function App() {
   const [ideas, setIdeas] = useState([]);
+  const [error, setError] = useState('')
 
   function addIdea(newIdea) {
-    setIdeas([...ideas, newIdea]);
-  };
+    fetch('http://localhost:3001/api/v1/ideas', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newIdea), 
+    })
+    .then(response => response.json())
+    .then(data => setIdeas([...ideas, data]))
+    .catch(error => setError(error.message)) 
+  }
 
   const deleteIdea = (id) => {
     const filteredIdeas = ideas.filter((idea) => idea.id !== id);
     setIdeas(filteredIdeas);
   };
 
+  function getIdeas() {
+    fetch('http://localhost:3001/api/v1/ideas')
+    .then(response => response.json())
+    .then(data => setIdeas([...ideas, ...data]))
+    .catch(error => {
+      console.log(error)
+      setError('Oops! Something went wrong! Please try again in a couple minutes.')
+    })
+  }
+
+  useEffect(() => {
+    getIdeas();
+  }, [])
+
   return (
     <main className='App'>
       <h1>IdeaBox</h1>
       <Form addIdea={addIdea} />
-      {!ideas.length && <p>Hmm...it feels empty in here. Let's add some ideas!</p>}
+      {error && <h2>{error}</h2>}
       <Ideas ideas={ideas} deleteIdea={deleteIdea} />
     </main>
   );
